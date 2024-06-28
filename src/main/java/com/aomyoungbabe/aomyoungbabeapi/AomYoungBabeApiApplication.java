062c45ec08a8b5f139226d6220b4beb9f621e13d
Tara <tarathep.siripis@gmail.com>
2024-06-28 16:03:19 +0700

    🥚 [ADD] Initial project

---
+++ src/main/java/com/aomyoungbabe/aomyoungbabeapi/AomYoungBabeApiApplication.java
@@ -0,0 +1,13 @@
+package com.aomyoungbabe.aomyoungbabeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AomYoungBabeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AomYoungBabeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aomyoungbabe/aomyoungbabeapi/AomYoungBabeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aomyoungbabe.aomyoungbabeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AomYoungBabeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
